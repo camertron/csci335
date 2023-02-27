430a84b94955c69970fe8c7fa6b8007a74af6113
Cameron Dutro <camertron@gmail.com>
2023-02-27 06:10:38 +0000

    Add more to the expenses skeleton code

---
+++ expenses-tracker/database.sql
@@ -0,0 +1,1 @@
+CREATE DATABASE `shared_expenses`;
--- expenses-tracker/web.py
+++ expenses-tracker/web.py
@@ -1,5 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqldb://root:password@127.0.0.1:3306/shared_expenses"
+db = SQLAlchemy(app)
 
 @app.route("/groups", methods=["POST"])
 def create_group():
