e5b35b4593a11e46a8f6e806cc6aaeb9fb2e9a14
Cameron Dutro <camertron@gmail.com>
2023-03-06 18:52:41 +0000

    Actually use the database in the shopping list compose example

---
+++ shopping-list-compose/01_create_items.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS `shopping_list`.`items` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `quantity` SMALLINT NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
