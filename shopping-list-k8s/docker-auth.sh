b9f2bd73459ea3ea03c4ad409c1cb4dc79392aeb
Cameron Dutro <camertron@gmail.com>
2023-04-05 17:51:07 +0000

    Install kubectl auth plugin for gcloud; add shopping-list-k8s code

---
+++ shopping-list-k8s/docker-auth.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+echo "Grabbing application default credentials (ADC)"
+gcloud auth login
+
+echo "Configuring Docker client to talk to GCP's Artifact Registry"
+gcloud auth configure-docker us-central1-docker.pkg.dev
+
+echo "Done!"
